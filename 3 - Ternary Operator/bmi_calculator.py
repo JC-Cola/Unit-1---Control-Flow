6dcd8e60e1daa86fc24f2f07d2fea1f5dc3c8d64
JC-Cola <jc.cola2020@gmail.com>
2025-09-22 09:01:50 -0400

    bmi calc done

--- 3 - Ternary Operator/bmi_calculator.py
+++ 3 - Ternary Operator/bmi_calculator.py
@@ -1,10 +1,11 @@
 weight = float(input("Enter your weight in pounds: "))
 height = float(input("Enter your height in inches: "))
-num = (weight/height^2) * 703
+num = (weight/(height*height)) * 703
 
 bmi = ("Underweight" if num < 18.5 else
        "Normal" if 18.5 <= num < 25 else
        "Overweight" if 25 <= num < 30 else
        "Obese"
        )
-print(f"Your BMI is {bmi}")
+print(f"Your BMI is {num}")
+print(f"You category is {bmi}")
